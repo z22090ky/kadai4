918463e44e0f2d45138fc80e60f0e95ba20e9312
Kouske Yoshida <z22090ky@aiit.ac.jp>
2022-08-02 22:27:55 +0900

    test.sh modified

--- kadai4-test.sh
+++ kadai4-test.sh
@@ -11,40 +11,40 @@ ERROR () {
 }
 
 # テスト開始
-echo "teat1-1: 正常系"
-./kadai4.sh 5 10 > $tmp-result
+echo "正常系1-1:引数が2つの自然数で第一引数の方が小さい場合"
+./kadai4.sh 4 6 > $tmp-result
+echo "2" > $tmp-anser
+diff $tmp-result $tmp-anser || echo "正常系1-1　エラー" >> $tmp-error
+
+echo "正常系1-2:引数が2つの自然数で第一引数の方が大きい場合"
+./kadai4.sh 35 20 > $tmp-result
 echo "5" > $tmp-anser
-diff $tmp-result $tmp-anser || echo "error in test1-1" >> $tmp-error
+diff $tmp-result $tmp-anser || echo "正常系1-2　エラー" >> $tmp-error
 
-echo "teat1-2: 正常系"
-./kadai4.sh 21 14 > $tmp-result
-echo "7" > $tmp-anser
-diff $tmp-result $tmp-anser || echo "error in test1-2" >> $tmp-error
-
-echo "teat2-1: 異常系(引数なし)"
+echo "異常系2-1:引数なし"
 ./kadai4.sh > $tmp-result
 echo "Error!" > $tmp-anser
-diff $tmp-result $tmp-anser || echo "error in test2-1" >> $tmp-error
+diff $tmp-result $tmp-anser || echo "異常系2-1　エラー　引数がありません" >> $tmp-error
 
-echo "teat2-2: 異常系(引数一つ)"
+echo "異常系2-2:引数が１つのみ"
 ./kadai4.sh 21 > $tmp-result
 echo "Error!" > $tmp-anser
-diff $tmp-result $tmp-anser || echo "error in test2-2" >> $tmp-error
+diff $tmp-result $tmp-anser || echo "異常系2-2　エラー　引数が1つしかありません" >> $tmp-error
 
-echo "teat2-3: 異常系(引数がマイナスの値)"
+echo "異常系2-3:i引数が1つかつ引数がマイナスの値"
 ./kadai4.sh -21 > $tmp-result
 echo "Error!" > $tmp-anser
-diff $tmp-result $tmp-anser || echo "error in test2-3" >> $tmp-error
+diff $tmp-result $tmp-anser || echo "異常系2-3　エラー　引数にマイナスの値を指定しています" >> $tmp-error
 
-echo "teat2-4: 異常系(引数がマイナスの値)"
+echo "異常系2-4:引数が2つかつ引数がマイナスの値"
 ./kadai4.sh 21 -1 > $tmp-result
 echo "Error!" > $tmp-anser
-diff $tmp-result $tmp-anser || echo "error in test2-4" >> $tmp-error
+diff $tmp-result $tmp-anser || echo "異常系2-4　エラー　引数にマイナスの値を指定してます" >> $tmp-error
 
-echo "teat2-5: 異常系(引数が文字列)"
+echo "異常系2-5: 引数が文字列"
 ./kadai4.sh "abc" > $tmp-result
 echo "Error!" > $tmp-anser
-diff $tmp-result $tmp-anser || echo "error in test2-5" >> $tmp-error
+diff $tmp-result $tmp-anser || echo "異常系2-5　エラー　引数に文字列が指定されています" >> $tmp-error
 
 # テストでエラーがあった場合はエラー出力
 if [ -f $tmp-error ]; then
